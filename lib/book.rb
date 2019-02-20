7b1424938c2a7d87b1d0a6dbcdc1d80946cb6dc9
rrosztoczy <rrosztoczy@gmail.com>
2019-02-20 13:51:03 -0500

    Updated shoe and book

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,29 @@
 
+# Book
+# ::new
+#   gets initialized with a title (FAILED - 1)
+# properties
+#   has a title (FAILED - 2)
+#   has an author name (FAILED - 3)
+#   has a page count (FAILED - 4)
+#   has a genre (FAILED - 5)
+# #turn_page
+#   can turn the page (FAILED - 6)
+
+
+class Book
+    def initialize(book_title)
+        @title = book_title
+    end
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+
 
